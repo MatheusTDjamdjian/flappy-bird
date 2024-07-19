9b1a1ff25ea39955bab69241da65f3aa8b9ab603
Matheus <matheustassodjamdjian@gmail.com>
2024-07-19 15:30:17 -0300

    ajustes

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const scoreDisplay = document.createElement('div');
     scoreDisplay.classList.add('score');
     gameDisplay.appendChild(scoreDisplay);
-
+  
     let birdLeft = 50;
     let birdBottom = 0;
     let jumpVelocity = 0;
@@ -22,147 +22,147 @@ document.addEventListener('DOMContentLoaded', () => {
     let pipes = [];
     let stars = [];
     let pipeSpeed = 20;
-
+  
     let gravity = 0.5;
     const initialJumpVelocity = 7;
-
+  
     playButton.addEventListener('click', startGame);
     resetButton.addEventListener('click', resetRecorde);
-
+  
     function startGame() {
-        bird.style.display = 'block';
-        birdBottom = 300;
-        birdLeft = 50;
-        isGameOver = false;
-        score = 0;
-        starScore = 0;
-        starPoints = 10;
-        passedPipes = 0;
-        pipeSpeed = 20;
-        menu.style.display = 'none';
-        scoreDisplay.style.display = 'block';
-        clearInterval(gameLoop);
-        gameLoop = setInterval(game, 8);
-        generatePipes();
+      bird.style.display = 'block';
+      birdBottom = 300;
+      birdLeft = 50;
+      isGameOver = false;
+      score = 0;
+      starScore = 0;
+      starPoints = 10;
+      passedPipes = 0;
+      pipeSpeed = 20;
+      menu.style.display = 'none';
+      scoreDisplay.style.display = 'block';
+      clearInterval(gameLoop);
+      gameLoop = setInterval(game, 8);
+      generatePipes();
     }
-
+  
     function resetRecorde() {
-        localStorage.setItem('recorde', 0);
-        updateRecorde();
+      localStorage.setItem('recorde', 0);
+      updateRecorde();
     }
-
+  
     function updateRecorde() {
-        const recorde = localStorage.getItem('recorde') || 0;
-        recordeDisplay.textContent = `Recorde: ${recorde}`;
-        recordeDisplay.classList.add('record-style');
+      const recorde = localStorage.getItem('recorde') || 0;
+      recordeDisplay.textContent = `Recorde: ${recorde}`;
+      recordeDisplay.classList.add('record-style');
     }
-
+  
     function updateScoreDisplay() {
-        scoreDisplay.textContent = `Pontuação: ${score}`;
+      scoreDisplay.textContent = `Pontuação: ${score}`;
     }
-
+  
     function game() {
-        birdBottom -= gravity;
-        bird.style.bottom = birdBottom + 'px';
-        bird.style.left = birdLeft + 'px';
-
-        if (birdBottom < 0) {
-            gameOver();
-        }
-
-        updateScoreDisplay();
-        checkCollision();
+      birdBottom -= gravity;
+      bird.style.bottom = birdBottom + 'px';
+      bird.style.left = birdLeft + 'px';
+  
+      if (birdBottom < 0) {
+        gameOver();
+      }
+  
+      updateScoreDisplay();
+      checkCollision();
     }
-
+  
     function checkCollision() {
-        const birdRect = bird.getBoundingClientRect();
-
-        pipes.forEach(pipe => {
-            const pipeRect = pipe.getBoundingClientRect();
-            const topPipe = pipe.querySelector('.top-pipe');
-            const bottomPipe = pipe.querySelector('.bottom-pipe');
-            const topPipeRect = topPipe.getBoundingClientRect();
-            const bottomPipeRect = bottomPipe.getBoundingClientRect();
-
-            if (
-                birdRect.left < topPipeRect.right &&
-                birdRect.right > topPipeRect.left &&
-                birdRect.top < topPipeRect.bottom &&
-                birdRect.bottom > topPipeRect.top
-            ) {
-                console.log("Colisão");
-                gameOver();
-            }
-
-            if (
-                birdRect.left < bottomPipeRect.right &&
-                birdRect.right > bottomPipeRect.left &&
-                birdRect.top < bottomPipeRect.bottom &&
-                birdRect.bottom > bottomPipeRect.top
-            ) {
-                console.log("Colisão");
-                gameOver();
-            }
-        });
-
-        stars.forEach(star => {
-            const starRect = star.getBoundingClientRect();
-            if (
-                birdRect.left < starRect.right &&
-                birdRect.right > starRect.left &&
-                birdRect.top < starRect.bottom &&
-                birdRect.bottom > starRect.top
-            ) {
-                score += starPoints;
-                starPoints += 10;
-                starScore++;
-                if (gameDisplay.contains(star)) {
-                    gameDisplay.removeChild(star);
-                }
-                stars = stars.filter(s => s !== star); 
-            }
-        });
+      const birdRect = bird.getBoundingClientRect();
+  
+      pipes.forEach(pipe => {
+        const pipeRect = pipe.getBoundingClientRect();
+        const topPipe = pipe.querySelector('.top-pipe');
+        const bottomPipe = pipe.querySelector('.bottom-pipe');
+        const topPipeRect = topPipe.getBoundingClientRect();
+        const bottomPipeRect = bottomPipe.getBoundingClientRect();
+  
+        if (
+          birdRect.left < topPipeRect.right &&
+          birdRect.right > topPipeRect.left &&
+          birdRect.top < topPipeRect.bottom &&
+          birdRect.bottom > topPipeRect.top
+        ) {
+          console.log("Colisão");
+          gameOver();
+        }
+  
+        if (
+          birdRect.left < bottomPipeRect.right &&
+          birdRect.right > bottomPipeRect.left &&
+          birdRect.top < bottomPipeRect.bottom &&
+          birdRect.bottom > bottomPipeRect.top
+        ) {
+          console.log("Colisão");
+          gameOver();
+        }
+      });
+  
+      stars.forEach(star => {
+        const starRect = star.getBoundingClientRect();
+        if (
+          birdRect.left < starRect.right &&
+          birdRect.right > starRect.left &&
+          birdRect.top < starRect.bottom &&
+          birdRect.bottom > starRect.top
+        ) {
+          score += starPoints;
+          starPoints += 10;
+          starScore++;
+          if (gameDisplay.contains(star)) {
+            gameDisplay.removeChild(star);
+          }
+          stars = stars.filter(s => s !== star);
+        }
+      });
     }
-
+  
     function jump() {
-        if (isGameOver) return;
-        birdBottom += jumpVelocity;
-        jumpVelocity -= gravity;
-
-        if (birdBottom < 0) {
-            birdBottom = 0;
-            jumpVelocity = 0;
-        }
+      if (isGameOver) return;
+      birdBottom += jumpVelocity;
+      jumpVelocity -= gravity;
+  
+      if (birdBottom < 0) {
+        birdBottom = 0;
+        jumpVelocity = 0;
+      }
     }
-
+  
     function startJump() {
-        jumpVelocity = initialJumpVelocity;
+      jumpVelocity = initialJumpVelocity;
     }
-
+  
     document.addEventListener('keyup', (e) => {
-        if (e.keyCode === 32) {
-            startJump();
-        }
+      if (e.keyCode === 32) {
+        startJump();
+      }
     });
-
+  
     setInterval(() => {
-        jump();
-        if (!isGameOver) {
-            console.log(birdBottom);
-        }
+      jump();
+      if (!isGameOver) {
+        console.log(birdBottom);
+      }
     }, 20);
-
-    function randomIntFromInterval(min, max) { 
-        return Math.floor(Math.random() * (max - min + 1) + min);
+  
+    function randomIntFromInterval(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
     }
-
+  
     function generatePipes() {
-        const pipe = document.createElement('div');
-        const topPipe = document.createElement('img');
-        const bottomPipe = document.createElement('img');
-
-        if (!isGameOver) {
-            pipe.classList.add('pipe');
+      const pipe = document.createElement('div');
+      const topPipe = document.createElement('img');
+      const bottomPipe = document.createElement('img');
+  
+      if (!isGameOver) {
+        pipe.classList.add('pipe');
             topPipe.src = 'img/toppipe.png';
             topPipe.classList.add('top-pipe');
             bottomPipe.src = 'img/bottompipe.png';
